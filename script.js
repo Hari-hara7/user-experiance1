ab0b4203e0f65caa63981284a68bfa5b911bafbf
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2024-01-26 10:09:49 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,9 @@
+function openWebsite(url) {
+    window.open(url, '_blank');
+}
+function openWebsite(url) {
+    window.open(url, '_blank');
+}
+function openWebsite(url) {
+    window.open(url, '_blank');
+}
