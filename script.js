a6ae5d57fa47607527daf73168011eb4b5f41bf7
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2024-03-10 22:38:46 +0530

    changed  text

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function openWebsite(url) {
-    window.open(url, '_blank');
-}
-function openWebsite(url) {
-    window.open(url, '_blank');
-}
-function openWebsite(url) {
-    window.open(url, '_blank');
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
+  const body = document.body;
+
+  toggleButton.addEventListener("click", function () {
+    body.classList.toggle("white-bg");
+
+    // Toggle text visibility and color
+    const textElements = document.querySelectorAll(".white-text");
+    textElements.forEach((element) => {
+      element.classList.toggle("hidden");
+    });
+  });
+});
+
